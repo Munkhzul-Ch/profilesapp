292898df0a67c74eb965bc31064681eec79ae688
Munkhzul Chogsom <munkhzulchogsom@Munkhzuls-MacBook-Pro.local>
2026-08-18 08:35:39 +1000

    displaying user profile

---
+++ amplify/auth/post-confirmation/handler.ts
@@ -0,0 +1,27 @@
+import type { PostConfirmationTriggerHandler } from "aws-lambda";
+import type { Schema } from "../../data/resource";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import { getAmplifyDataClientConfig } from "@aws-amplify/backend/function/runtime";
+import { env } from "$amplify/env/post-confirmation";
+
+const { resourceConfig, libraryOptions } =
+  await getAmplifyDataClientConfig(env);
+
+Amplify.configure(resourceConfig, libraryOptions);
+
+const client = generateClient<Schema>();
+
+export const handler: PostConfirmationTriggerHandler = async (event) => {
+  const { errors } = await client.models.UserProfile.create({
+    email: event.request.userAttributes.email,
+    profileOwner: `${event.request.userAttributes.sub}::${event.userName}`,
+  });
+
+  if (errors) {
+    console.error(errors);
+    throw new Error("Failed to create user profile");
+  }
+
+  return event;
+};
